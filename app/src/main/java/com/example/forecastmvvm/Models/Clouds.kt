1c04390d7f06a5e882c7cb41ba5d51a212ddfbdf
GUL KARAKAS <sugarless.syke@gmail.com>
2023-09-12 22:24:48 +0300

    Initial commit

---
+++ app/src/main/java/com/example/forecastmvvm/Models/Clouds.kt
@@ -0,0 +1,5 @@
+package com.example.forecastmvvm.Models
+
+data class Clouds(
+    val all: Int
+)
---
+++ app/src/main/java/com/example/forecastmvvm/Models/Rain.kt
@@ -0,0 +1,5 @@
+package com.example.forecastmvvm.Models
+
+data class Rain(
+    val `1h`: Double
+)
